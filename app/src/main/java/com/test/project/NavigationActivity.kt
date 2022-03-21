fcdad62e5b7d430aaa8f350634037d8ab3f04879
Tim <ska1v3r@gmail.com>
2022-03-21 21:51:03 +0700

    init commit

---
+++ app/src/main/java/com/test/project/NavigationActivity.kt
@@ -0,0 +1,19 @@
+package com.test.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+
+class NavigationActivity : AppCompatActivity(R.layout.activity_navigation){
+
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+
+}
+
+
+
