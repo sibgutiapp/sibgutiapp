744601588300d07c61c10d0c7bff523047ac34e1
thekravchan <thekravchan@gmail.com>
2022-03-24 01:04:20 +0700

    Navigation Component beed added

--- app/src/main/java/com/test/project/NavigationActivity.kt
+++ app/src/main/java/com/test/project/NavigationActivity.kt
@@ -3,10 +3,13 @@ package com.test.project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.viewbinding.library.activity.viewBinding
+import com.test.project.databinding.ActivityNavigationBinding
 
 
 class NavigationActivity : AppCompatActivity(R.layout.activity_navigation){
 
+    private val binding: ActivityNavigationBinding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
