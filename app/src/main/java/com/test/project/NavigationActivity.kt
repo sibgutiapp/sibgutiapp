5139b2eab0350f89476ee2a8482b78330408123c
Fadin Vladislav <sad_456@ngs.ru>
2022-03-25 01:42:38 +0700

    Add splashcreen

--- app/src/main/java/com/test/project/NavigationActivity.kt
+++ app/src/main/java/com/test/project/NavigationActivity.kt
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.viewbinding.library.activity.viewBinding
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.test.project.databinding.ActivityNavigationBinding
 
 
-class NavigationActivity : AppCompatActivity(R.layout.activity_navigation){
+class NavigationActivity : AppCompatActivity(){
 
     private val binding: ActivityNavigationBinding by viewBinding()
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
 
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_navigation)
     }
 
 }
