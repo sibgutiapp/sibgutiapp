f40421125f1953ea8cca44234fd5246d2ec9fc7d
Fadin Vladislav <sad_456@ngs.ru>
2022-04-11 18:54:13 +0700

    Add navigateUp button

--- app/src/main/java/com/test/project/NavigationActivity.kt
+++ app/src/main/java/com/test/project/NavigationActivity.kt
@@ -5,8 +5,10 @@ import android.view.View
 import android.viewbinding.library.activity.viewBinding
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.test.project.databinding.ActivityNavigationBinding
@@ -15,6 +17,7 @@ import com.test.project.databinding.ActivityNavigationBinding
 class NavigationActivity : AppCompatActivity() {
 
     private val binding: ActivityNavigationBinding by viewBinding()
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +34,17 @@ class NavigationActivity : AppCompatActivity() {
                 else -> hideBottomNavigationMenu()
             }
         }
-        val appBarConfiguration =
-            AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.bottomNavigationMenu.setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
 
     private fun initBottomMenu() {
         binding.bottomNavigationMenu.itemIconTintList = null
