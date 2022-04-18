9a0b18167ebfb563677acf0fb22c1f60dfbe6b7d
Andrey Dubrovin <verssy236@gmail.com>
2022-04-18 19:02:53 +0700

    add recycler into full news fragment

--- app/src/main/java/com/test/project/ui/home/fullnews/FullNewsListAdapter.kt
+++ app/src/main/java/com/test/project/ui/home/fullnews/FullNewsListAdapter.kt
@@ -1,33 +1,66 @@
 package com.test.project.ui.home.fullnews
 
+import android.annotation.SuppressLint
+import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.test.project.databinding.FullNewsFragmentBinding
+import com.test.project.databinding.ItemFullNewsListBinding
+import com.test.project.domain.entity.News
+import com.test.project.ui.home.HomeNewsListAdapter
 
-class FullNewsListAdapter : RecyclerView.Adapter<FullNewsListAdapter.ViewHolder>() {
-    class ViewHolder(private val binding: FullNewsFragmentBinding) :
+class FullNewsListAdapter :
+    RecyclerView.Adapter<FullNewsListAdapter.ViewHolder>() {
+
+    lateinit var listener: OnItemClickListener
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+
+    private var dataList: MutableList<News> = mutableListOf()
+
+    fun setUpdatedData(dataList: List<News>) {
+        this.dataList.clear()
+        this.dataList.addAll(dataList)
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(private val binding: ItemFullNewsListBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind() {
-
+        fun bind(data: News) {
+            with(binding) {
+                textviewItemTitle.text = data.title
+                textviewItemTitle.typeface = Typeface.DEFAULT_BOLD
+                textviewItemAuthor.text = data.author?.fullName ?: ""
+                textviewItemDate.text = data.dateTime
+            }
+            itemView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
         }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind()
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
-            FullNewsFragmentBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
+            ItemFullNewsListBinding.inflate(
+                LayoutInflater.from(viewGroup.context),
+                viewGroup,
                 false
             )
         )
     }
 
-    override fun getItemCount(): Int {
-        return 0
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.bind(dataList[position])
     }
+
+    override fun getItemCount() = dataList.size
 }
