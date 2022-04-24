54abee888c2df08c00a57dfb8bdefaf82fef90b3
Fadin Vladislav <sad_456@ngs.ru>
2022-04-24 23:50:00 +0700

    Implemented room

--- app/src/main/java/com/test/project/App.kt
+++ app/src/main/java/com/test/project/App.kt
@@ -1,13 +1,13 @@
 package com.test.project
 
 import android.app.Application
+import com.test.project.data.dataSource.database.NewsDatabase
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
-class App: Application() {
-
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
