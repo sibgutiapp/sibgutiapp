43be0e14ab35cae25325de00c866a980c241e195
Fadin Vladislav <sad_456@ngs.ru>
2022-05-09 19:47:08 +0700

    Add register button

--- app/src/main/java/com/test/project/ui/login/LoginFragment.kt
+++ app/src/main/java/com/test/project/ui/login/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.test.project.ui.login
 
-import android.app.ActionBar
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.viewbinding.library.fragment.viewBinding
@@ -9,12 +10,26 @@ import androidx.navigation.fragment.findNavController
 import com.test.project.R
 import com.test.project.databinding.LoginFragmentBinding
 
-class LoginFragment: Fragment(R.layout.login_fragment) {
+
+class LoginFragment : Fragment(R.layout.login_fragment) {
     private val binding: LoginFragmentBinding by viewBinding()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.buttonLogin.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
         }
+        with(binding.buttonRegister) {
+            setOnClickListener {
+                register()
+            }
+            stateListAnimator = null
+            setBackgroundColor(requireContext().getColor(R.color.white))
+        }
+    }
+
+    private fun register() {
+        val url = "https://sibsutis.ru/join"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        requireActivity().startActivity(intent)
     }
 }
