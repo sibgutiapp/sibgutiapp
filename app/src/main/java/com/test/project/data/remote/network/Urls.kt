eac00a7dd19ed3a8474e7f95b687410feeec160c
sibgutiapp <51432987+sibgutiapp@users.noreply.github.com>
2022-05-19 07:04:47 +0700

    new url

--- app/src/main/java/com/test/project/data/remote/network/Urls.kt
+++ app/src/main/java/com/test/project/data/remote/network/Urls.kt
@@ -3,7 +3,7 @@ package com.test.project.data.remote.network
 sealed class Urls {
 
     class RELEASE: Urls() {
-        val BASE_URL = "https://sibguti.herokuapp.com/"
+        val BASE_URL = "https://sibgutiapp.herokuapp.com/"
     }
 
 }
